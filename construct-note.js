a41e469d9d176f085639bbdf86d3889c113ee3c8
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-09 15:59:29 -0800

    impletmented function and passed test

--- construct-note.js
+++ construct-note.js
@@ -1,2 +1,22 @@
 // add whatever parameters you deem necessary
-function constructNote() { }
+function freqCounter(str) {
+    let obj = {};
+    for (let char of str) {
+        obj[char] = (obj[char] + 1) || 1;
+    }
+    return obj;
+}
+
+function constructNote(msg, letters) {
+
+    if (msg === "") return true;
+    let message = freqCounter(msg);
+    let l = freqCounter(letters);
+
+    for (let key of Object.keys(message)) {
+        if (message[key] <= l[key]) {
+            return true;
+        }
+    }
+    return false;
+}
