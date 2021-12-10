a472fcb5b0210f3a89a6784bd15176d7a1c3b77b
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-09 18:52:57 -0800

    calculated average of array

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,23 @@
 // add whatever parameters you deem necessary
-function averagePair() {
+function averagePair(arr, value) {
+
+    let left = 0;
+    let right = arr.length - 1;
+    let avg = 0;
+
+    while (left < right) {
+        avg = (arr[left] + arr[right]) / 2;
+        if (avg === value) {
+            left++;
+            right--;
+            return true;
+        }
+        else if (avg < value) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return false;
 }
