d9b2e6365b187656b44045fa7da086aedf7ede76
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-09 17:32:23 -0800

    implemented function

--- count-pairs.js
+++ count-pairs.js
@@ -1,3 +1,35 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+// function countPairs(arr, val) {
+//     let pair = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             let sum = arr[i] + arr[j];
+//             if (sum === val) {
+//                 pair++;
+//             }
+//         }
+//     } return pair;
+// }
+
+function countPairs(arr, value) {
+    arr.sort((a, b) => a - b);
+    let left = 0;
+    let right = arr.length - 1;
+    let pair = 0, sum = 0;
+
+    while (left < right) {
+        sum = arr[left] + arr[right];
+        if (sum === value) {
+            pair++;
+            left++;
+            right--;
+        }
+        else if (sum < value) {
+            left++;
+        }
+        else {
+            right--;
+        }
+    }
+    return pair;
 }
