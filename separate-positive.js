5a9f6c2d28fa2deb908c16cfdd0c8e0dda411d9c
poorvarathore05 <poorvarathore05@gmail.com>
2021-12-09 18:53:33 -0800

    separate positive number

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,27 @@
 // add whatever parameters you deem necessary
-function separatePositive() {
+function separatePositive(arr) {
+
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+
+        if (arr[left] < 0 && arr[right] > 0) {
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left += 1;
+            right -= 1;
+        }
+        else {
+            if (arr[left] > 0) {
+                left += 1;
+            }
+            else {
+                right -= 1;
+            }
+        }
+    }
+    return arr;
+
 }
